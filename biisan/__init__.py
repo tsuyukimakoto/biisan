afcbfc7f605df64cd9f24ceec67131d72d28cb50
Makoto Tsuyuki <mtsuyuki@gmail.com>
2019-10-25 14:04:02 +0900

    need fork, changed default on macOS

--- biisan/__init__.py
+++ biisan/__init__.py
@@ -1,6 +1,9 @@
 import os
+import multiprocessing
 
 version_info = (0, 8, 1)
 __version__ = ".".join([str(v) for v in version_info])
 
 os.environ['GLUE_PLATE_BASE_MODULE'] = 'biisan.biisan_settings'
+
+multiprocessing.set_start_method('fork')
